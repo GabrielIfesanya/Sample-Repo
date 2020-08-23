0880dac9d23599979244e02236c1074469f2ee11
Gabriel Ifesanya <gabrielifesanya@hotmail.com>
2020-08-23 21:19:07 +0100

    Changed Code

--- script.py
+++ script.py
@@ -1,10 +1,7 @@
-import math
-import os
-import sys
 
 import requests
 
 def test():
     r = requests.get("http://google.com")
-    test = 'Test'
-print(r.status_code)
+    print(r.status_code)
+    print(r.ok)
